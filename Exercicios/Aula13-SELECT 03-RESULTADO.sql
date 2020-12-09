d8684f3c966dd3f7d030b90656996f21b29a38c3
Lucas Samuel <lfrlicianodasilva@gmail.com>
2020-12-09 18:58:04 -0300

    update

--- Exercicios/Aula13-SELECT 03-RESULTADO.sql
+++
@@ -1,7 +0,0 @@
-select profissao, count(*) from gafanhotos group by profissao order by count(*);
-
-select sexo, count(*) from gafanhotos where nascimento > '2005/01/01' group by sexo;
-
-select nacionalidade, count(*) from gafanhotos where nacionalidade not like 'Brasil' group by nacionalidade having count(*) > 3;
-
-select altura, count(*) from gafanhotos where peso > '100' group by altura having altura > (select avg(altura) from gafanhotos);
